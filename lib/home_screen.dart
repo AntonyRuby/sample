4e456a2a23300ef7c1b3bb1bb0a86718417151d5
Antony Ruby Alexia <alexia@m2p.in>
2021-06-19 22:05:59 +0530

    App version display completed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:package_info/package_info.dart';
 import 'package:sample/qr_scan_screen/qr_scan_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,19 +11,36 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Home Screen"),
-          actions: [
-            IconButton(
-                icon: Icon(Icons.forward),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => QrcodeScannerScreen()));
-                })
-          ],
-        ),
-        body: Column());
+      appBar: AppBar(
+        title: Text("Home Screen"),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.forward),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => QrcodeScannerScreen()));
+              })
+        ],
+      ),
+      body: FutureBuilder<PackageInfo>(
+        future: PackageInfo.fromPlatform(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              return Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  'Version: ${snapshot.data.version}',
+                ),
+              );
+
+            default:
+              return const SizedBox();
+          }
+        },
+      ),
+    );
   }
 }
